8abb8a97d453989c1748069c6b273259a9148a24
Fennec <fennec@flolon.cc>
2024-06-05 16:41:57 -0400

    Finish theme selector, delete unused image

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,23 @@
+function updateThemeSelector() {
+	let themeSelector = $("#changeThemeBtn");
+	const svg = document.createElement("svg");
+	if (localStorage.getItem("theme") === "default-theme") {
+		svg.innerHTML = darkTheme;
+	} else {
+		svg.innerHTML = lightTheme;
+	}
+	themeSelector.querySelector("svg").remove();
+	themeSelector.append(svg);
+}
+
 $("#changeThemeBtn").addEventListener("click", (e) => {
-    if (localStorage.getItem("theme") === "default-theme") {
-        localStorage.setItem("theme", "dark-theme");
-        document.querySelector("#changeThemeBtn svg").innerHtml = lightTheme
-    } else {
-        localStorage.setItem("theme", "default-theme");
-    }
-    refreshTheme();
+	if (localStorage.getItem("theme") === "default-theme") {
+		localStorage.setItem("theme", "dark-theme");
+	} else {
+		localStorage.setItem("theme", "default-theme");
+	}
+	updateThemeSelector();
+	refreshTheme();
 });
+
+updateThemeSelector();
