6fda4432fd12364e1b1c094f38dacf68d00fd98c
Fennec Border <me@flolon.cc>
2024-06-04 23:21:41 -0400

    wip. change icon for theme select btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 $("#changeThemeBtn").addEventListener("click", (e) => {
     if (localStorage.getItem("theme") === "default-theme") {
         localStorage.setItem("theme", "dark-theme");
+        document.querySelector("#changeThemeBtn svg").innerHtml = lightTheme
     } else {
-        localStorage.setItem("theme", "default-theme"); 
+        localStorage.setItem("theme", "default-theme");
     }
     refreshTheme();
 });
